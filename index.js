76f3b872b2ef8296889b03aef296860ee681bd6e
zanybird <saifur583509@gmail.com>
2023-11-26 21:27:18 +0600

    inseted the user in the database

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 // middleware 
 app.use(cors({
-    origin: ['https://bejewelled-dragon-b28d12.netlify.app'],
+    origin: ['http://localhost:5173'],
     credentials: true
 }));
 app.use(express.json());
@@ -52,76 +52,77 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7) await
         client.connect();
 
-        const addedJob = client.db('jobDb').collection('job');
-        const orderBid = client.db('jobDb').collection('bid');
+        // const addedJob = client.db('jobDb').collection('job');
+        // const orderBid = client.db('jobDb').collection('bid');
+        const addUser = client.db('usersDb').collection('user');
 
         // auth related api jwt 
-        app.post('/jwt', async (req, res) => {
-            const user = req.body;
-            console.log(user);
-            const token = jwt.sign(user, 'process.env.ACCESS_TOKEN_SECRET', { expiresIn: '1h' })
-            res
-                .cookie('token', token, {
-                    httpOnly: true,
-                    secure: process.env.NODE_ENV === 'production',
-                    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-                })
-                .send({ success: true })
-        })
+        // app.post('/jwt', async (req, res) => {
+        //     const user = req.body;
+        //     console.log(user);
+        //     const token = jwt.sign(user, 'process.env.ACCESS_TOKEN_SECRET', { expiresIn: '1h' })
+        //     res
+        //         .cookie('token', token, {
+        //             httpOnly: true,
+        //             secure: process.env.NODE_ENV === 'production',
+        //             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        //         })
+        //         .send({ success: true })
+        // })
 
         // secure: process.env.NODE_ENV === 'production', 
         // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
 
-        app.post('/jwt', async (req, res) => {
-            const signInUser = req.body;
-            console.log(signInUser);
-            const token = jwt.sign(signInUser, 'process.env.ACCESS_TOKEN_SECRET', { expiresIn: '1h' })
-            res
-                .cookie('token', token, {
-                    httpOnly: true,
-                    secure: process.env.NODE_ENV === 'production',
-                    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-                })
-                .send({ success: true })
-        })
+        // app.post('/jwt', async (req, res) => {
+        //     const signInUser = req.body;
+        //     console.log(signInUser);
+        //     const token = jwt.sign(signInUser, 'process.env.ACCESS_TOKEN_SECRET', { expiresIn: '1h' })
+        //     res
+        //         .cookie('token', token, {
+        //             httpOnly: true,
+        //             secure: process.env.NODE_ENV === 'production',
+        //             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        //         })
+        //         .send({ success: true })
+        // })
 
-        app.get('/webCategory', async (req, res) => {
-            const cursor = addedJob.find({ category: "Web Design and Development" });
-            const result = await cursor.toArray();
-            res.send(result);
-        })
-        app.get('/graphicsCategory', async (req, res) => {
-            const cursor = addedJob.find({ category: "Graphics Design" });
-            const result = await cursor.toArray();
-            res.send(result);
-        })
-        app.get('/marketingCategory', async (req, res) => {
-            const cursor = addedJob.find({ category: "Digital Marketing" });
-            const result = await cursor.toArray();
-            res.send(result);
-        })
+        // app.get('/webCategory', async (req, res) => {
+        //     const cursor = addedJob.find({ category: "Web Design and Development" });
+        //     const result = await cursor.toArray();
+        //     res.send(result);
+        // })
+        // app.get('/graphicsCategory', async (req, res) => {
+        //     const cursor = addedJob.find({ category: "Graphics Design" });
+        //     const result = await cursor.toArray();
+        //     res.send(result);
+        // })
+        // app.get('/marketingCategory', async (req, res) => {
+        //     const cursor = addedJob.find({ category: "Digital Marketing" });
+        //     const result = await cursor.toArray();
+        //     res.send(result);
+        // })
 
-        app.get('/Jobs', async (req, res) => {
-            console.log(req.query.email)
-            let query = {};
-            if (req.query?.email) {
-                query = { email: req.query.email }
-            }
-            const cursor = addedJob.find(query);
-            const result = await cursor.toArray();
-            res.send(result);
-        })
+        // app.get('/Jobs', async (req, res) => {
+        //     console.log(req.query.email)
+        //     let query = {};
+        //     if (req.query?.email) {
+        //         query = { email: req.query.email }
+        //     }
+        //     const cursor = addedJob.find(query);
+        //     const result = await cursor.toArray();
+        //     res.send(result);
+        // })
 
-        app.get('/jobs', async (req, res) => {
-            const cursor = addedJob.find();
-            const result = await cursor.toArray();
-            res.send(result);
-        })
+        // app.get('/jobs', async (req, res) => {
+        //     const cursor = addedJob.find();
+        //     const result = await cursor.toArray();
+        //     res.send(result);
+        // })
 
-        app.post('/jobs', async (req, res) => {
-            const newJob = req.body;
-            console.log(newJob)
-            const result = await addedJob.insertOne(newJob);
+        app.post('/users', async (req, res) => {
+            const newUser =req.body;
+            console.log(newUser)
+            const result = await addUser.insertOne(newUser);
             res.send(result)
         })
 
@@ -134,119 +135,119 @@ async function run() {
 
         // up to date jobs 
 
-        app.put('/jobs/:id', async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: new ObjectId(id) };
-            const options = { upsert: true };
-            const updateJob = req.body;
-            const job = {
-                $set: {
-                    title: updateJob.title,
-                    photo: updateJob.photo,
-                    minimumPrice: updateJob.minimumPrice,
-                    maximumPrice: updateJob.maximumPrice,
-                    category: updateJob.category,
-                    deadline: updateJob.deadline,
-                    Description: updateJob.Description
-                }
-            }
-            const result = await addedJob.updateOne(filter, job, options);
-            res.send(result)
-        })
+        // app.put('/jobs/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const filter = { _id: new ObjectId(id) };
+        //     const options = { upsert: true };
+        //     const updateJob = req.body;
+        //     const job = {
+        //         $set: {
+        //             title: updateJob.title,
+        //             photo: updateJob.photo,
+        //             minimumPrice: updateJob.minimumPrice,
+        //             maximumPrice: updateJob.maximumPrice,
+        //             category: updateJob.category,
+        //             deadline: updateJob.deadline,
+        //             Description: updateJob.Description
+        //         }
+        //     }
+        //     const result = await addedJob.updateOne(filter, job, options);
+        //     res.send(result)
+        // })
 
         // delete crud operation 
 
-        app.delete('/jobs/:id', async (req, res) => {
-            const id = req.params.id;
-            console.log(id)
-            const query = { _id: new ObjectId(id) };
-            const result = await addedJob.deleteOne(query);
-            res.send(result)
-        })
+        // app.delete('/jobs/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     console.log(id)
+        //     const query = { _id: new ObjectId(id) };
+        //     const result = await addedJob.deleteOne(query);
+        //     res.send(result)
+        // })
 
         // order bid section 
-        app.post('/bids', async (req, res) => {
-            const bidJob = req.body;
-            console.log(bidJob)
-            const result = await orderBid.insertOne(bidJob);
-            res.send(result)
-        })
+        // app.post('/bids', async (req, res) => {
+        //     const bidJob = req.body;
+        //     console.log(bidJob)
+        //     const result = await orderBid.insertOne(bidJob);
+        //     res.send(result)
+        // })
 
-        app.get('/bids', async (req, res) => {
-            console.log(req.query.email)
-            console.log('got token from client site', req.cookies.token)
-            let query = {};
-            if (req.query?.email) {
-                query = { email: req.query.email }
-            }
-            const cursor = orderBid.find(query);
-            const result = await cursor.toArray();
-            res.send(result);
-        })
+        // app.get('/bids', async (req, res) => {
+        //     console.log(req.query.email)
+        //     console.log('got token from client site', req.cookies.token)
+        //     let query = {};
+        //     if (req.query?.email) {
+        //         query = { email: req.query.email }
+        //     }
+        //     const cursor = orderBid.find(query);
+        //     const result = await cursor.toArray();
+        //     res.send(result);
+        // })
 
-        app.get('/bids', async (req, res) => {
-            const bidItems = req.body;
-            console.log(bidItems)
-            const result = await orderBid.find().toArray();
-            res.send(result);
-        })
+        // app.get('/bids', async (req, res) => {
+        //     const bidItems = req.body;
+        //     console.log(bidItems)
+        //     const result = await orderBid.find().toArray();
+        //     res.send(result);
+        // })
 
-        app.get('/bids/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await orderBid.findOne(query);
-            res.send(result)
-        })
+        // app.get('/bids/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: new ObjectId(id) };
+        //     const result = await orderBid.findOne(query);
+        //     res.send(result)
+        // })
 
-        app.patch('/bids/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const updateBid = req.body;
-            console.log(updateBid);
-            const updateDoc = {
-                $set: {
-                    status: updateBid.status
-                },
-            };
-            const result = await orderBid.updateOne(query, updateDoc);
-            res.send(result)
-        })
+        // app.patch('/bids/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: new ObjectId(id) };
+        //     const updateBid = req.body;
+        //     console.log(updateBid);
+        //     const updateDoc = {
+        //         $set: {
+        //             status: updateBid.status
+        //         },
+        //     };
+        //     const result = await orderBid.updateOne(query, updateDoc);
+        //     res.send(result)
+        // })
 
-        app.patch('/bids/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const updateBidReject = req.body;
-            console.log(updateBid);
-            const updateDoc = {
-                $set: {
-                    status: updateBidReject.reject
-                },
-            };
-            const result = await orderBid.updateOne(query, updateDoc);
-            res.send(result)
-        })
+        // app.patch('/bids/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: new ObjectId(id) };
+        //     const updateBidReject = req.body;
+        //     console.log(updateBid);
+        //     const updateDoc = {
+        //         $set: {
+        //             status: updateBidReject.reject
+        //         },
+        //     };
+        //     const result = await orderBid.updateOne(query, updateDoc);
+        //     res.send(result)
+        // })
 
-        app.patch('/bids/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const updateComplete = req.body;
-            console.log(updateBid);
-            const updateDoc = {
-                $set: {
-                    status: updateComplete.complete
-                },
-            };
-            const result = await orderBid.updateOne(query, updateDoc);
-            res.send(result)
-        })
+        // app.patch('/bids/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: new ObjectId(id) };
+        //     const updateComplete = req.body;
+        //     console.log(updateBid);
+        //     const updateDoc = {
+        //         $set: {
+        //             status: updateComplete.complete
+        //         },
+        //     };
+        //     const result = await orderBid.updateOne(query, updateDoc);
+        //     res.send(result)
+        // })
 
-        app.delete('/bids/:id', async (req, res) => {
-            const id = req.params.id;
-            console.log(id)
-            const query = { _id: new ObjectId(id) };
-            const result = await orderBid.deleteOne(query);
-            res.send(result)
-        })
+        // app.delete('/bids/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     console.log(id)
+        //     const query = { _id: new ObjectId(id) };
+        //     const result = await orderBid.deleteOne(query);
+        //     res.send(result)
+        // })
 
 
 
@@ -261,7 +262,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send("The assignment-11 server is running")
+    res.send("The assignment-12 server is running")
 })
 
 app.listen(port, () => {
